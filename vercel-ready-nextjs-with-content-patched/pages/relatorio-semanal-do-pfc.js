b490d033daadd800cfcb60913cc0c6fac391c4fb
tnk-bea <beatrizct@estudante.ufscar.br>
2025-08-31 20:47:08 -0300

    Delete vercel-ready-nextjs-with-content-patched directory

--- vercel-ready-nextjs-with-content-patched/pages/relatorio-semanal-do-pfc.js
+++
@@ -1,9 +0,0 @@
-import Head from 'next/head';
-import '/styles/globals.css';
-export default function Page(){
-  return (<div className="container">
-    <Head><title>Relatório semanal do PFC</title><meta name="viewport" content="width=device-width, initial-scale=1" /></Head>
-    <h1>Relatório semanal do PFC</h1>
-    <article dangerouslySetInnerHTML={__html: ``} />
-  </div>);
-}
